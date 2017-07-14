5a005397d03347f9363f550cf60cc245a6a8751d
israel_wong <israel_wong@calypso.com>
2017-07-14 18:27:55 +0800

    20170714 1827
    Change-Id: I0be08a57e5d3f42ee0c95c4401d46917a403d4d7

---
+++ src/com/ridiculands/TestOCA.java
@@ -0,0 +1,43 @@
+package com.ridiculands;
+
+public class TestOCA {
+	private static int $;
+	public static void main(String[] args) {
+		// 1
+		String a_b = null;
+//		System.out.print($);
+//		System.out.print(a_b);
+
+		// 4
+		boolean keepGoing = true;
+		int count = 0;
+		int x = 3;
+		while (count++ < 3) {
+			int y = (1 + 2 * count) % 3;
+//			System.out.println("y: " + y);
+			switch(y) {
+				default:
+				case 0: x -= 1; break;
+				case 1: x += 5;
+			}
+//			System.out.println("x: " + x);
+		}
+
+		// 5
+		System.out.println("A");
+		try {
+			System.out.println("B");
+			throw new IllegalArgumentException();
+		} catch (RuntimeException e) {
+			System.out.println("C");
+		} finally {
+			System.out.println("D");
+		}
+		System.out.println("E");
+	}
+	
+	public interface Animal { public String getName(); }
+	interface Mammal { public default String getName() { return null; } }
+	abstract class Otter implements Mammal, Animal {}
+
+}
