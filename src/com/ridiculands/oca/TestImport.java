f3012ea6db77a2baa708f07f6832f03d8eecc003
israel_wong <israel_wong@calypso.com>
2017-07-27 15:46:51 +0800

    20170727 1546
    Change-Id: I1d7987abde5e0ddd2bd2923e2a5ae1d8a8a57a9e

---
+++ src/com/ridiculands/oca/TestImport.java
@@ -0,0 +1,16 @@
+package com.ridiculands.oca;
+
+//import java.util.*;
+//import java.sql.*;
+
+import java.util.Date;
+//import java.sql.Date;
+
+public class TestImport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date date = null;
+	}
+
+}
