3029334e3a764c8a3880da04ea19b1aa8b3573da
Pangyan Wong <pangyan.wong@gmail.com>
2021-12-05 17:44:18 +0800

    CLS-167 - automated tests

--- src/test/com/ridiculands/dto/TestSerialization.java
+++ src/test/com/ridiculands/dto/TestSerialization.java
@@ -3,6 +3,7 @@ package com.ridiculands.dto;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
@@ -17,15 +18,29 @@ public class TestSerialization {
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    public void testSerialize() throws Exception {
+    public void testSerializeCat() throws Exception {
         Cat cat = new Cat("Tom", 5, new BigDecimal("30"));
         String json = objectMapper.writeValueAsString(cat);
-        System.err.println(json);
+        assertThat(json, equalTo(CAT_JSON));
     }
 
     @Test
-    public void testDeserializeCat() throws Exception {
-        LandAnimal animal = objectMapper.readValue(CAT_JSON, LandAnimal.class);
+    public void testSerializeDog() throws Exception {
+        Dog dog = new Dog("Amy", 2, new BigDecimal("40"));
+        String json = objectMapper.writeValueAsString(dog);
+        assertThat(json, equalTo(DOG_JSON));
+    }
+
+    @Test
+    public void testSerializeTurtle() throws Exception {
+        Turtle turtle = new Turtle("Andrew", 7, new BigDecimal("15"));
+        String json = objectMapper.writeValueAsString(turtle);
+        assertThat(json, equalTo(TURTLE_JSON));
+    }
+
+    @Test
+    public <E extends LandAnimal> void testDeserializeCat() throws Exception {
+        E animal = objectMapper.readValue(CAT_JSON, new TypeReference<>() {});
         animal.talk();
         assertThat(animal, isA(Cat.class));
         assertThat(animal, hasProperty("name", equalTo("Tom")));
@@ -37,12 +52,19 @@ public class TestSerialization {
     public void testDeserializeDog() throws Exception {
         LandAnimal animal = objectMapper.readValue(DOG_JSON, LandAnimal.class);
         animal.talk();
+        assertThat(animal, isA(Dog.class));
+        assertThat(animal, hasProperty("name", equalTo("Amy")));
+        assertThat(animal, hasProperty("age", equalTo(2)));
+        assertThat(animal, hasProperty("bodyLength", comparesEqualTo(new BigDecimal("40.00"))));
     }
 
     @Test
     public void testDeserializeTurtle() throws Exception {
         LandAnimal animal = objectMapper.readValue(TURTLE_JSON, LandAnimal.class);
         animal.talk();
+        assertThat(animal, isA(Turtle.class));
+        assertThat(animal, hasProperty("name", equalTo("Andrew")));
+        assertThat(animal, hasProperty("age", equalTo(7)));
+        assertThat(animal, hasProperty("bodyLength", comparesEqualTo(new BigDecimal("15.00"))));
     }
-
 }
