81ce7faafbdc28e7323c74cae82fc7a8b4ea3e9f
Pangyan Wong <pangyan.wong@gmail.com>
2023-05-29 22:51:06 +0800

    CLS-167 - test nio

--- src/test/com/ridiculands/TestNio.java
+++ src/test/com/ridiculands/TestNio.java
@@ -1,2 +1,18 @@
-package com.ridiculands;public class TestNio {
+package com.ridiculands;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.stream.Collectors;
+
+public class TestNio {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("/Users/pywong/Documents/EPAM/rm1");
+        for (Path entry : Files.walk(path).collect(Collectors.toList())) {
+            BasicFileAttributes bfa = Files.readAttributes(entry, BasicFileAttributes.class);
+            System.err.println(entry.getFileName().getFileName() + " " + bfa.creationTime() + " " + bfa.isDirectory() + " " + bfa.isRegularFile());
+        }
+    }
 }
